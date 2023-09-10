af9665acaeb835276707fa7b8062a8c29919fab6
Euan Muir <unterm30@gmail.com>
2023-09-10 14:50:16 +0100

    Fix alias

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -19,7 +19,7 @@ type -q zoxide; and zoxide init fish | source
 
 # Alias/abbr
 # ----------------------------------------------------------------
-type -q codium; and alias codium="flatpak run com.vscodium.codium --no-sandbox"
+alias codium "flatpak run com.vscodium.codium --no-sandbox"
 type -q lsd; and alias ls 'lsd'
 type -q bat; and abbr cat bat
 type -q batcat; and abbr cat batcat
